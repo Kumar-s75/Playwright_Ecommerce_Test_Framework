11b947b4bdb23bf8a65fa93659dd590b992bf1b8
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-08-16 00:06:50 +0530

    setting up playwright project

---
+++ playwright.config.js
@@ -0,0 +1,41 @@
+const { defineConfig } = require('@playwright/test');
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  retries: 1,
+   workers: 1,
+  //testMatch: 'tests/InvalidLogin.spec.js',
+  //by default wait tym is 30000
+  timeout: 60000,
+  //Assertion level tymout
+  expect:
+  {
+    timeout: 60000,
+  },
+
+ reporter: [
+    ['allure-playwright', {
+      detail: true,
+      outputFolder: 'allure-results',   // Raw results go here
+      suiteTitle: false,
+    }],
+    ['list']                             // Console output
+  ],
+  use: {
+    browserName: 'chromium',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
+  },
+
+//Testing Branching functionality
+  
+});
+
+  
+
